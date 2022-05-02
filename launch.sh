a8ecd846ee6fd486254e48673d7b4d5273d1738d
Dean Sparrow <dean@deansparrow.com>
2022-05-02 22:55:38 +0000

    add pivx-testnet conf

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bin/blockbook -sync -resyncindexperiod=60017 -resyncmempoolperiod=60017 -blockchaincfg=build/blockchaincfg.json -internal=:9049 -public=:443  -logtostderr -certfile=/etc/letsencrypt/live/explorer.rockdev.org
+bin/blockbook -sync -resyncindexperiod=60017 -resyncmempoolperiod=60017 -blockchaincfg=build/blockchaincfg.json -internal=:9049 -public=:443  -logtostderr 
